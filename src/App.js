be1e63899c4cd32d16af1a3de9bb7482b7f35d30
yan530 <yan530@uw.edu>
2018-11-15 15:10:18 -0800

    added header, main and footer

--- src/App.js
+++ src/App.js
@@ -1,2 +1,20 @@
 import React, { Component } from 'react';
+import './style.css';
 
+class App extends Component {
+    render() {
+        return (
+            <div className='home'>
+                <header aria-label="contains the navbar of the page">
+                <h1>This is the hiking project</h1>
+                </header>
+                <main aria-label="contains the main content of the page">
+                </main>
+                <footer aria-label="This is the contact info">
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default App;
