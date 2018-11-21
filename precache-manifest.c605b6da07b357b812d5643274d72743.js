fccdc9a7e7d55e9909e35d75728850eff41bc709
harim2000 <harimsanchez@gmail.com>
2018-11-21 00:27:27 -0800

    Updates

---
+++ precache-manifest.c605b6da07b357b812d5643274d72743.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "350eaf5ad51dbb7d6a85b8fef5d03db6",
+    "url": "/project-kylegoodwin/static/media/nature2-small.350eaf5a.jpg"
+  },
+  {
+    "revision": "2ca4cb85411897562296f05d6043b257",
+    "url": "/project-kylegoodwin/static/media/hiker-mini.2ca4cb85.jpg"
+  },
+  {
+    "revision": "59a1fd31a3c3a21e59de46b2aa3ef13d",
+    "url": "/project-kylegoodwin/static/media/Hamburger.59a1fd31.svg"
+  },
+  {
+    "revision": "f55913bb20fb5d8d2ce7",
+    "url": "/project-kylegoodwin/static/js/runtime~main.f55913bb.js"
+  },
+  {
+    "revision": "bd012de99125893e256d",
+    "url": "/project-kylegoodwin/static/js/main.bd012de9.chunk.js"
+  },
+  {
+    "revision": "c8be67061e3c96cf55fa",
+    "url": "/project-kylegoodwin/static/js/1.c8be6706.chunk.js"
+  },
+  {
+    "revision": "bd012de99125893e256d",
+    "url": "/project-kylegoodwin/static/css/main.30c912f7.chunk.css"
+  },
+  {
+    "revision": "c8be67061e3c96cf55fa",
+    "url": "/project-kylegoodwin/static/css/1.92fa37fa.chunk.css"
+  },
+  {
+    "revision": "5d00979109799311186921f72b330705",
+    "url": "/project-kylegoodwin/index.html"
+  }
+];
