58c71fd3f18198e4b4d006c7ac8c419971281056
Kyle Goodwin <kjgoodwins@gmail.com>
2018-11-23 00:20:54 -0800

    first filter working

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ class App extends Component {
             easy: true,
             medium: true,
             hard: true,
-            error: false
+            error: ""
         }
 
     }
 
-    search = (term, easy, medium, hard, error) => {
+    search = (term, error) => {
 
 
         let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + term + '&key=AIzaSyBifT_4HbuyAHS6I01s-4ZRjO_P5F3plGg';
@@ -38,9 +38,10 @@ class App extends Component {
        })
        .then((data) => {
            let address = data.results[0].geometry;
-            this.getLocation(address.location.lat,address.location.lng, easy, medium, hard, error);
+            this.getLocation(address.location.lat,address.location.lng, error);
        })
        .catch((err) => {
+           console.log("here");
            this.getError(err);
        });
     }
@@ -61,29 +62,40 @@ class App extends Component {
         })
     }
 
-    getFilters = (easy, medium, hard) =>{
-        this.setState(
-            {
-            easy: easy,
-            medium: medium,
-            hard: hard
-            }
-        )
+    getFilter = (target) => {
+        let name = target.name;
+        if (target.checked) {
+            this.setState({ [name]: target.value });
+        } else {
+            this.setState({ [name]: !target.value });
+        }
     }
 
     render() {
+        let error = this.state.error !== "" ? <div className="error-message">{this.state.error}</div>: null;
         return (
             <div className='home'>
                 <NavBar />
-                {this.state.error && <div className="error-message">Address not found</div>}
-                <Header searchTerm={this.state.searchTerm} lat={this.state.lat} 
-                lng={this.state.lng} howToSearch={this.search} getLocation={this.getLocation} 
-                easy={this.state.easy} medium={this.state.medium} hard={this.state.hard}
-                error={this.state.error}/>
-                <Main searchTerm={this.state.searchTerm} lat={this.state.lat} lng={this.state.lng} 
-                maxDist={this.state.maxDist} maxResults={this.state.maxResults}
-                easy={this.state.easy} medium={this.state.medium} hard={this.state.hard}
-                error={this.state.error}/>
+                <Header 
+                    searchTerm={this.state.searchTerm} 
+                    lat={this.state.lat} 
+                    lng={this.state.lng} 
+                    howToSearch={this.search} 
+                    getFilter={this.getFilter} 
+                    getLocation={this.getLocation} 
+                    easy={this.state.easy} 
+                    medium={this.state.medium} 
+                    hard={this.state.hard}
+                    error={this.state.error} 
+                    getError={this.getError}/>
+                {error}
+                <Main searchTerm={this.state.searchTerm} 
+                    lat={this.state.lat} lng={this.state.lng} 
+                    maxDist={this.state.maxDist} 
+                    maxResults={this.state.maxResults}
+                    easy={this.state.easy} 
+                    medium={this.state.medium} 
+                    hard={this.state.hard}/>
                 <Footer />
             </div>
         )
--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ export class Main extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { trailData: {}, displayedTrails: {} };
+        this.state = { trailData: {}, displayedTrails: {}, newLocation: false };
     }
 
     //Search term from form is passed in as this.props.searchTerm
@@ -30,30 +30,34 @@ export class Main extends Component {
                         displayedTrails: data.trails
                     }
                 );
+                this.diffFilter();
             })
             .catch(function () {
             });
     }
 
-    diffFilter(diffFilter){
+    diffFilter(){
         let hikesToDisplay = this.state.trailData;
-    /*
-        if( !diffFilter.easy){
-            hikesToDisplay.filter( (hike) => {
+    
+        if( !this.props.easy){
+            hikesToDisplay = hikesToDisplay.filter( (hike) => {
                 return !(hike.difficulty === "green" || hike.difficulty === "greenBlue")
             });
-        }else if( !diffFilter.medium){
-            hikesToDisplay.filter( (hike) => {
+        }
+        
+        if( !this.props.medium){
+            hikesToDisplay = hikesToDisplay.filter( (hike) => {
                 return !(hike.difficulty === "blue" || hike.difficulty === "blueBlack")
             });
-        }else if (!diffFilter.hard){
-            hikesToDisplay.filter( (hike) => {
+        }
+        
+        if (!this.props.hard){
+            hikesToDisplay = hikesToDisplay.filter( (hike) => {
                 return !(hike.difficulty === "black" || hike.difficulty === "blackBlack")
             });
         }
-
         this.setState( { displayedTrails: hikesToDisplay} );
-        */
+        
 
 
     }
@@ -67,27 +71,29 @@ export class Main extends Component {
             this.getData(this.props.lat, this.props.lng, this.props.maxDist, this.props.maxResults);
         }
 
-        console.log(this.props.easy,this.props.medium,this.props.hard);
+        if(prevProps.easy !== this.props.easy || prevProps.medium !== this.props.medium || prevProps.hard != this.props.hard){
+            this.diffFilter();
+        }
 
     }
-
 
     render() {
         let error;
         if (this.state.trailData.length === 0) {
             error = <div className="error-message">Cannot find hikes :(</div>;
         }
-
-
         return (
             <main aria-label="contains the main content of the page">
                 {error}
                 <div className="section">
-                    <MapArea lat={this.props.lat} lng={this.props.lng} trails={this.state.trailData} />
+                    <MapArea lat={this.props.lat} lng={this.props.lng} trails={this.state.displayedTrails} />
                 </div>
 
                 <div className="section">
-                    <CardContainer trails={this.state.trailData} />
+                    <CardContainer trails={this.state.displayedTrails} 
+                    easy={this.props.easy} 
+                    medium={this.props.medium} 
+                    hard={this.props.hard} />
                 </div>
             </main>
         )
