772f6acea38a350fcc6ce0a0e0d72da0377a5025
Kateka Seth <katekaseth@gmail.com>
2018-11-28 18:07:08 -0800

    update firebase account

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase/app'
 import 'firebase/auth';
 
-// Initialize Firebase
+  // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBu6aas5spE_4NzbcMShkKUxffyQwKXF2o",
-    authDomain: "info-340.firebaseapp.com",
-    databaseURL: "https://info-340.firebaseio.com",
-    projectId: "info-340",
-    storageBucket: "info-340.appspot.com",
-    messagingSenderId: "833380606572"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBrDLqpmG_aZMsAUj2ei_CMi8yk1OZWHoM",
+  authDomain: "hiking-project-db0fd.firebaseapp.com",
+  databaseURL: "https://hiking-project-db0fd.firebaseio.com",
+  projectId: "hiking-project-db0fd",
+  storageBucket: "hiking-project-db0fd.appspot.com",
+  messagingSenderId: "605830719119"
+};
+firebase.initializeApp(config);
   
 ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL+'/'}><App /></BrowserRouter>, document.getElementById('root'));
 
