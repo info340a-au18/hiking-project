4421387de18a5224bdd6a0429bf69d9b88c7c5a7
yan530 <yan530@uw.edu>
2018-11-29 20:55:32 -0800

    scrolling map

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './HomePage.scss';
-import firebase from 'firebase/app';
 import {Header} from './Header';
 import {Main} from './Main';
 
