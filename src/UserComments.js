dc18b23b302c6cadda7d0be5f271edc8f248de0a
Kateka Seth <katekaseth@gmail.com>
2018-12-04 21:05:06 -0800

    add user reviews

--- src/UserComments.js
+++ src/UserComments.js
@@ -3,43 +3,69 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import './UserComments.css';
+import Moment from 'react-moment';
 
 export class UserComments extends Component {
-    componentDidMount() {
-        // if user is signed in or not
-        this.authUnRegFunc = firebase.auth().onAuthStateChanged((firebaseUser) => {
-            if(firebaseUser){ //signed in!
-              this.setState({user: firebaseUser});
-            } else { //signed out
-              this.setState({user: null});
-            }
-        });
-
-    //     // store the hike as the state, as well as hike comments
-    //     this.commentRef = firebase.database().ref('hikes/' + this.props.location.state.hike.id);
-    //     this.commentRef.on('value', (snapshot) => {
-    //         let val = snapshot.val();
-    //         let commentsArray = undefined;
-    //         if (val) {
-    //             let commentKeys = Object.keys(val);
-    //             commentsArray = commentKeys.map((key) => {
-    //                 let obj = val[key];
-    //                 obj.id = key;
-    //                 return obj;
-    //             })
-    //         }
-    //         this.setState({
-    //             trail: this.props.location.state.hike,
-    //             comments: commentsArray
-    //         });
-    //     });
+    constructor(props) {
+        super(props);
+        this.state = {comments:[]};
+        let user = this.props.user;
+        if (user) {
+            this.commentRef = firebase.database().ref('users/' + user.uid + '/userReviews');
+            this.commentRef.on('value', (snapshot) => {
+                let val = snapshot.val();
+                let commentsArray = undefined;
+                if (val) {
+                    let commentKeys = Object.keys(val);
+                    commentsArray = commentKeys.map((key) => {
+                        let obj = val[key];
+                        obj.id = key;
+                        return obj;
+                    })
+                }
+                this.setState({
+                    comments: commentsArray
+                });
+            });
+            
+        }
     }
 
     render() {
+        let comments = this.state.comments.map((item) => {
+            let itemKey = Object.keys(item)[0];
+            return item[itemKey];
+        })
+        let renderedComments = comments.map((item) => {
+            return (
+                
+                <div className="card mx-auto">
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            {item.displayName}
+                        </h5>
+                        <h6 className="card-subtitle mb-2 text-muted">
+                            <Moment date={item.time} fromNow></Moment>
+                        </h6>
+                        <p className="card-text">{item.text}</p>
+                    </div>
+                </div>
+                
+            );
+        })
         return (
             <div>
-                <h1>Your Reviews!</h1>
+                <h1>Your Reviews</h1>
 
+                <div class="col">
+                    {renderedComments}
+                </div>
+
+                {/* quick fix so footer doesn't cover comments */}
+                <div className="hidden">
+                    <p className="card-text">buffer</p>
+                    <p className="card-text">buffer</p>
+                </div>
             </div>
         );
     }
